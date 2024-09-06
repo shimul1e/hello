368f712b887fa365d624c8221a2547f40f8ed908
shimul1e <skshimul79977007@gmail.com>
2024-09-06 05:42:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+const express=require("express")
+
+const app=express()
+
+app.get("/",(req,res)=>{
+
+  res.send("Hello-world")
+  
+})
+
+app.listen(5000,()=>{
+  console.log("open server")
+})
+
+
+
+
+
